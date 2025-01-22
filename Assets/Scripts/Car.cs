9eca847930c21564e209ee928ae1e38516d9cd1c
신영룡 <86341272+0dragon@users.noreply.github.com>
2025-01-22 15:46:30 +0900

    버튼 누를 때만 옆으로 이동

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -1,17 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Car : MonoBehaviour
 {
     public float speed = 10.0f;
-    void Start()
-    {
-        
-    }
+    public float turnSpeed = 10.0f;
+    public float horizontalInput;
+    
     
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        
+        transform.Translate(Vector3.right * horizontalInput * turnSpeed * Time.deltaTime);
+    }
+
+    public void SetHorizontalInput(float input)
+    {
+        horizontalInput = input;
+    }
+
+    public void OnPointerDownLeft()
+    {
+        SetHorizontalInput(-1);
+    }
+
+    public void OnPointerDownRight()
+    {
+        SetHorizontalInput(1);
+    }
+
+    public void OnPointerUp()
+    {
+        SetHorizontalInput(0);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,18 +8,21 @@ public class GameManager : MonoBehaviour
     public GameObject player;
     public GameObject panel;
     public Button startButton;
-    // Start is called before the first frame update
+    
+    private Car carController;
+    
     void Start()
     {
-        player.GetComponent<Car>().enabled = false;
+        carController = player.GetComponent<Car>();
         
+        carController.enabled = false;
         startButton.onClick.AddListener(StartGame);
     }
 
     private void StartGame()
     {
         // 게임 시작 시 플레이어 움직임을 활성화합니다.
-        player.GetComponent<Car>().enabled = true;
+        carController.enabled = true;
         // 시작 버튼을 비활성화합니다.
         panel.SetActive(false);
         // startButton.gameObject.SetActive(false);
