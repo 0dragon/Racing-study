fd0ca4e5192fb0388147b722af804f7a1dd072e9
신영룡 <86341272+0dragon@users.noreply.github.com>
2025-01-24 16:05:58 +0900

    뇌정지on
    레이캐스트 관련 문제

--- Assets/Scripts/Game/GameTests/GameTestScript.cs
+++ Assets/Scripts/Game/GameTests/GameTestScript.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 public class GameTestScript
 {
+    private Car _car;
+    private GameObject _leftMoveButton;
+    private GameObject _rightMoveButton;
+    
     // A Test behaves as an ordinary method
     [Test]
     public void GameTestScriptSimplePasses()
@@ -32,22 +37,26 @@ public class GameTestScript
 
         var startButton = GameObject.Find("Btn_Start");
         Assert.IsNotNull(startButton, "StartButton is null");
-
+        
         // 게임 실행
         startButton.GetComponent<UnityEngine.UI.Button>().onClick.Invoke();
         
+        // 플레이어 차 확인
+        _car = GameObject.Find("Car").GetComponent<Car>();
+        Assert.IsNotNull(_car, "Car is null");
+        
         // 게임 제어 관련 버튼 클릭
-        var leftButton = GameObject.Find("Btn_Left");
-        Assert.IsNotNull(leftButton, "LeftButton is null");
-        var rightButton = GameObject.Find("Btn_Right");
-        Assert.IsNotNull(rightButton, "RightButton is null");
+        _leftMoveButton = GameObject.Find("Btn_Left");
+        Assert.IsNotNull(_leftMoveButton, "LeftButton is null");
+        _rightMoveButton = GameObject.Find("Btn_Right");
+        Assert.IsNotNull(_rightMoveButton, "RightButton is null");
         
         // 가스의 등장 위치 파악
-        Vector3 leftPosition = new Vector3(-1f, 0.2f, -3f);
-        Vector3 rightPosition = new Vector3(1f, 0.2f, -3f);
-        Vector3 centerPosition = new Vector3(0, 0.2f, -3f);
+        Vector3 leftPosition = new Vector3(-5f, 0.2f, 0f);
+        Vector3 rightPosition = new Vector3(5f, 0.2f, 0f);
+        Vector3 centerPosition = new Vector3(0, 0.2f, 0f);
 
-        float rayDistance = 10f;
+        float rayDistance = 20f;
         Vector3 rayDirection = Vector3.forward;
 
         var InGamePanel = GameObject.Find("InGamePanel");
@@ -58,20 +67,25 @@ public class GameTestScript
             RaycastHit hit;
             if (Physics.Raycast(leftPosition, rayDirection, out hit, rayDistance, LayerMask.GetMask("Gas")))
             {
-                Debug.Log("left");
+                Debug.Log("left" + hit.point);
+                MoveCar(hit.point);
             }
             else if (Physics.Raycast(rightPosition, rayDirection, out hit, rayDistance,
                          LayerMask.GetMask("Gas")))
             {
-                Debug.Log("right");
+                Debug.Log("right" + hit.point);
+                MoveCar(hit.point);
             }
             else if (Physics.Raycast(centerPosition, rayDirection, out hit, rayDistance, LayerMask.GetMask("Gas")))
             {
-                Debug.Log("center");
+                Debug.Log("center" + hit.point);
+                MoveCar(hit.point);
             }
             else
             {
-                Debug.Log("none");
+                Debug.Log("none" + hit.point);
+                MoveButtonUp(_leftMoveButton);
+                MoveButtonUp(_rightMoveButton);
             }
             
             Debug.DrawRay(leftPosition, rayDirection * rayDistance, Color.red);
@@ -86,9 +100,45 @@ public class GameTestScript
 
     private IEnumerator waitForSceneLoad()
     {
-        while (SceneManager.GetActiveScene().buildIndex > 0)
+        while (!SceneManager.GetActiveScene().isLoaded)
         {
             yield return null;
         }
+    }
+
+    private void MoveButtonDown(GameObject moveButton)
+    {
+        PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+        ExecuteEvents.Execute(moveButton.gameObject, pointerEventData, ExecuteEvents.pointerDownHandler);
+    }
+    
+    private void MoveButtonUp(GameObject moveButton)
+    {
+        PointerEventData pointerEventData = new PointerEventData(EventSystem.current);
+        ExecuteEvents.Execute(moveButton.gameObject, pointerEventData, ExecuteEvents.pointerUpHandler);
+    }
+
+    private void MoveCar(Vector3 targetPosition)
+    {
+        if (Mathf.Abs(targetPosition.x - _car.transform.position.x) < 0.01f)
+        {
+            MoveButtonUp(_leftMoveButton);
+            MoveButtonUp(_rightMoveButton);
+            return;
+        }
+        
+        if (targetPosition.x < _car.transform.position.x)
+        {
+            // 왼쪽으로 이동
+            MoveButtonDown(_leftMoveButton);
+            MoveButtonUp(_rightMoveButton);
+        }
+        else if (targetPosition.x > _car.transform.position.x)
+        {
+            // 오른쪽으로 이동
+            MoveButtonDown(_rightMoveButton);
+            MoveButtonUp(_leftMoveButton);
+        }
+        Debug.Log("Car position after move: " + _car.transform.position);
     }
 }
